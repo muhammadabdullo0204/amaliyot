5d23e26da2db9c6ed92e543805b556e5a2bee7ec
Muhammadabdullo Ziyodulloyev <muhammadabdullo0204@gmail.com>
2024-01-30 17:34:31 +0500

    done

--- main.ts
+++ main.ts
@@ -1,1 +1,142 @@
-console.log("helo");
+// $(document).ready(function () {
+//     var imageIndex = 1;
+//     updateNavbarBackground(imageIndex);
+  
+//     setInterval(function () {
+//       updateNavbarBackground(++imageIndex);
+//     }, 1000);
+//   });
+  
+//   function updateNavbarBackground(index) {
+//     var images = ["slider1.jpg", "slider2.jpg", "slider3.jpg"];
+//     var navbarElement = $("#navbar");
+  
+//     if (index > images.length) {
+//       imageIndex = 1;
+//     }
+//     if (index < 1) {
+//       imageIndex = images.length;
+//     }
+  
+//     var imageUrl = "url(" + images[imageIndex - 1] + ")";
+//     navbarElement.css("background-image", imageUrl);
+//   }
+  
+  
+
+$(document).ready(function () {
+
+    function initializeSlider(images, sliderId) {
+        var currentIndex = 0;
+
+        $(sliderId).click(function () {
+            
+            $(".navbar").css({
+                "background-image": "url('images/" + images[currentIndex] + "')",
+                "background-repeat": "no-repeat",
+                "background-size": "cover",
+                "width": "100%",
+                "height": "95vh",
+                "transition": "background-image 0.1s ease-in-out"
+            });
+            currentIndex = (currentIndex + 1) % images.length;
+        });
+
+
+    }
+
+    var slider1Images = ["slider1.jpg"];
+    initializeSlider(slider1Images, "#slider1");
+
+    var slider2Images = ["slider2.jpg"];
+    initializeSlider(slider2Images, "#slider2");
+
+    var slider3Images = ["slider3.jpg"];
+    initializeSlider(slider3Images, "#slider3");
+
+
+
+});
+
+$(document).ready(function () {
+    $("#hotel-option").mouseover(function () {
+        $("#a").css({
+            "display": "block"
+        });
+    });
+
+    $("#hotel-option").click(function () {
+        $("#a").css({
+            "display": "none"
+        });
+    });
+});
+
+$(document).ready(function () {
+    $("#king-hotel").mouseover(function () {
+        $("#b").css({
+            "display": "block"
+        });
+    });
+
+    $("#king-hotel").click(function () {
+        $("#b").css({
+            "display": "none"
+        });
+    });
+
+});
+
+
+
+
+
+
+
+
+
+$(document).ready(function () {
+
+    $("#slider1").click(function(){
+
+        
+        $("#slider2, #slider3").not(this).css({
+            "border": "none"
+        });
+
+        $(this).css({
+            "border": "1px solid white"
+        });
+    });
+
+    $("#slider2").click(function(){
+
+        $("#slider-text-1").text("MAKE YOUR VACATION COMFORTABLE");
+        $("#slider-text").text("deluxe hotel");
+
+
+        $("#slider1, #slider3").not(this).css({
+            "border": "none"
+        });
+
+        $(this).css({
+            "border": "1px solid white"
+        });
+    });
+
+    $("#slider3").click(function(){
+
+        $("#slider-text-1").text("A BEST PLACE TO ENJOY YOUR LIFE");
+        $("#slider-text").text("LUXE HOTEL");
+
+        $("#slider1, #slider2").not(this).css({
+            "border": "none"
+        });
+
+        $(this).css({
+            "border": "1px solid white"
+        });
+    });
+});
+
+
---
+++ main2.js
@@ -0,0 +1,45 @@
+var sliderText = document.getElementById("slider-text");
+var sliderText1 = document.getElementById("slider-text-1");
+function change1() {
+    sliderText.innerText = "BORA HOTEL";
+    sliderText1.innerText = "RESERVE ROOM FOR FAMILY VACTION";
+}
+function change2() {
+    sliderText.innerText = "DELUXE HOTEL";
+    sliderText1.innerText = "MAKE YOUR VACATION COMFORTABLE";
+}
+function change3() {
+    sliderText.innerText = "LUXE HOTEL";
+    sliderText1.innerText = "A BEST PLACE TO ENJOY YOUR  LIFE ";
+}
+var menuToggle = document.querySelector('.menuToggle');
+var sidebar = document.querySelector(".sidebar");
+var sidebarText = document.querySelector(".sidebar-text");
+menuToggle.onclick = function () {
+    menuToggle.classList.toggle('active');
+    if (sidebar.style.width === "300px") {
+        sidebar.style.width = "0px";
+        sidebarText.style.transform = "translateX(0)";
+        sidebarText.style.transform = "translateY(100px)";
+    }
+    else {
+        sidebar.style.width = "300px";
+        sidebarText.style.transform = "translateX(0px)";
+        sidebarText.style.transform = "translateY(100px)";
+    }
+};
+// let sidebar = document.getElementById("#menuToggle");
+// sidebar?.onclick = function(){
+// }
+// let navbar = document.getElementById("navbar");
+// let imageIndex = 0;
+// const images = [
+//     "url('images/slider1.jpg')", 
+//     "url('images/slider2.jpg')",
+//     "url('images/slider3.jpg')",
+// ];
+// function changeBackground() {
+//     navbar.style.backgroundImage = images[imageIndex];
+//     imageIndex = (imageIndex + 1) % images.length;
+// }
+// setInterval(changeBackground, 4000);
